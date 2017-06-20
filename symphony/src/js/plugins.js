a9c3dbc9b15fe2c82b41d643b3423e2eef6e7845
Young9789 <youngjoo@compsolution.net>
2017-06-20 16:45:26 -0700

    Test Project
    Test project

---
+++ symphony/src/js/plugins.js
@@ -0,0 +1,16 @@
+/* jquery */
+var jQuery = require('jquery');
+
+/* jquery easing */
+var easing = require('jquery.easing');
+
+/* jquery Swiper plugin */
+var swiper = require('swiper');
+
+// /* daterangepicker js */
+var daterangepicker = require('bootstrap-daterangepicker');
+
+global.window.jQuery = jQuery;
+global.window.easing = easing;
+global.window.swiper = swiper;
+global.window.daterangepicker = daterangepicker;
